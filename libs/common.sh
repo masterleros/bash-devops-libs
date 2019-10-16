77365e5fdc14156ebd266ecfa20f81d6dcd87fa6
Leonardo Morales <leonardo.morales@gft.com>
2019-10-16 18:45:38 +0200

    Update libs/common.sh, libs/gcp.sh files Deleted libs/error.sh

--- libs/common.sh
+++ libs/common.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ROOTDIR="$(cd $(dirname ${BASH_SOURCE[0]})/../ >/dev/null 2>&1 && pwd )"
 
 ### Exit program with text when last exit code is non-zero ###
 # usage: exitOnError <output_message> [optional: forced code (defaul:exit code)]
@@ -13,3 +14,31 @@ function exitOnError {
     fi
 }
 
+# Execute a command until success within a retries
+function retryExecution {
+    retries=${1}
+    shift
+    cmd=${@}
+
+    for retry in $(seq $((${retries}+1))); do
+        eval "${cmd}"
+        if [ ${?} -eq 0 ]; then
+            return 0
+        elif [ ${retry} -ne $((${retries}+1)) ]; then
+            echo "Retying(${retry}) execution of '${cmd}'..."
+        fi        
+    done
+
+    # if could not be sucess after retries
+    exitOnError "The command '${cmd}' could not be executed successfuly after ${retries} retry(s)" -1
+}
+
+# Call the desired function when script is invoked directly instead of included
+if [ $(basename $0) == $(basename ${BASH_SOURCE[0]}) ]; then
+    function=${1}
+    shift
+    $function "${@}"
+fi
+
+# Validate if OS is supported
+[ "${OSTYPE}" != "linux-gnu" ] || exitOnError "OS '${OSTYPE}' is not supported" -1
--- libs/gcp.sh
+++ libs/gcp.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+source $(dirname ${BASH_SOURCE[0]})/common.sh
 
+echo "Hello from '${BASH_SOURCE[0]}'"
