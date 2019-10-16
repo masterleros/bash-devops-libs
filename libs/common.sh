3ed20296bd34991d8d283f5364fc922c14a1b326
Leonardo Morales <leonardo.morales@gft.com>
2019-10-16 19:09:41 +0200

    Update libs/common.sh, libs/gcp.sh files

--- libs/common.sh
+++ libs/common.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello from '${BASH_SOURCE[0]}'"
+#echo "Hello from '${BASH_SOURCE[0]}'"
 
 ROOTDIR="$(cd $(dirname ${BASH_SOURCE[0]})/../ >/dev/null 2>&1 && pwd )"
 
--- libs/gcp.sh
+++ libs/gcp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
+#echo "Hello from '${BASH_SOURCE[0]}'"
 source $(dirname ${BASH_SOURCE[0]})/common.sh
-echo "Hello from '${BASH_SOURCE[0]}'"
