5af867e256db30263189bb4da6aeed63e120ce00
Leonardo Morales <leonardo.morales@gft.com>
2019-10-23 22:20:05 +0200

    Update gitlab-libs.sh, templates/.gcp-template.yml files

--- gitlab-libs.sh
+++ gitlab-libs.sh
@@ -5,7 +5,8 @@
 #
 
 ### GITLAB LIBS DEFINITIONS ###
-GITLAB_LIBS_REPO="git.gft.com:devops-br/gitlab-gft-libs.git"
+GITLAB_LIBS_SERVER="git.gft.com"
+GITLAB_LIBS_REPO="devops-br/gitlab-gft-libs.git"
 GITLAB_LIBS_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)/libs"
 GITLAB_TMP_DIR="/tmp/gitlab-gft-libs"
 ### GITLAB LIBS DEFINITIONS ###
@@ -19,24 +20,26 @@ if [[ "${OSTYPE}" != "linux-gnu" ]]; then echo "OS '${OSTYPE}' is not supported"
 # Check if git is present
 if [ $(which git &> /dev/null || echo $?) ]; then echo 'ERROR: git command is required!' >&2; exit -1; fi
 
+# Check if the execution in on GitLab
+if [ "$CI" ]; then echo "---> Running on GitLab CI/CD <---"; fi
+
 ### Clone / update the libraries ###
 set -e
 if [ ! -d ${GITLAB_TMP_DIR} ]; then
     if [ ${CI_JOB_TOKEN} ]; then 
-        git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@${GITLAB_LIBS_REPO} ${GITLAB_TMP_DIR}
+        git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@${GITLAB_LIBS_SERVER}/${GITLAB_LIBS_REPO} ${GITLAB_TMP_DIR}
     else
-        git clone git@${GITLAB_LIBS_REPO} ${GITLAB_TMP_DIR}
+        git clone git@${GITLAB_LIBS_SERVER}:${GITLAB_LIBS_REPO} ${GITLAB_TMP_DIR}
     fi
 else
     git -C ${GITLAB_TMP_DIR} pull 
 fi
-set +e
 
-### Copy the libs inside the project ###
+### Create dir and copy the libs inside the project ###
+mkdir -p ${GITLAB_LIBS_DIR}
 cp -r ${GITLAB_TMP_DIR}/libs/* ${GITLAB_LIBS_DIR}
 
-# Make all libs executable
-find ${GITLAB_LIBS_DIR} -name 'main.sh' -exec chmod +x {} \
+set +e
 
 ### Include GitLab Libs ###
 source ${GITLAB_LIBS_DIR}/common.sh
