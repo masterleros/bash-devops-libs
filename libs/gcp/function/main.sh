d517a21b2235cba7500f641fe6dfad6a151de5d0
Andre Prado <andre.prado@gft.com>
2020-01-22 13:10:15 +0100

    Updates

--- libs/gcp/function/main.sh
+++ libs/gcp/function/main.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-### # Get all Cloud Function definition files ###
-# usage: getValuesFromFile <file> <var1> <var2> ... <varN>
+### Find all Cloud Function definition files ###
+# usage: findDefinitions <absolute functions folder path> <cloud function definition filename>
 function findDefinitions() {
     FUNCTIONS_FOLDER=${1}
     CF_DEFINITIONS_FILE=${2}
 
     #CF_DEF_FILES=($(find ${FUNCTIONS_FOLDER} -iname ${CF_DEFINITIONS_FILE} -printf '%P\n' | sort))
     CF_DEF_FILES=($(find ${FUNCTIONS_FOLDER} -iname ${CF_DEFINITIONS_FILE} | sort))
-    [ "${#CF_DEF_FILES[@]}" -eq 0 ] && exitOnError "Unable to locate any configuration file (${CF_DEFINITIONS_FILE}) to deploy the cloud function. File should be availabel under <ROOTDIR>/functions/<folder>/" -1
+    [ "${#CF_DEF_FILES[@]}" -eq 0 ] && exitOnError "Unable to locate any configuration file (${CF_DEFINITIONS_FILE}) to deploy the cloud function. File should be availabel under ${FUNCTIONS_FOLDER}/<folder>/" -1
    
 }
 
+### Deploy Cloud Function ###
+# usage: deploy <function name> <aditional parameters>
+function deploy() {
+    getArgs "cfName parameters" "${@}"
+    echo "gcloud functions deploy ${cfName} ${parameters} | grep -vi password"
+    #exitOnError "Failed to deploy GCP Function ${cfName}"
+}
+
+### Returns all Cloud Function definition files found in findDefinitions function ###
+# usage: getDefinitions
 function getDefinitions() {
     [ "${#CF_DEF_FILES[@]}" -eq 0 ] && exitOnError "No definitions available. Please, make sure to call the do.gcp.function.findDefinitions before this one" -1
     echo ${CF_DEF_FILES[@]}
 }
-
-#function deploy() {
-    # to be defined
-#}
-
-#function prepareForDeploy() {
-
-    #getArgs "FUNCTIONS_FOLDER CF_DEFINITIONS_FILE" ${@}
-    #findDefinitions
-    
-#}
