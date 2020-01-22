9f9927f65661a51c7f280929ffa2dc1fb72084f8
Andre Prado <andre.prado@gft.com>
2020-01-22 15:59:11 +0100

    Updates

--- libs/gcp/function/main.sh
+++ libs/gcp/function/main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Import required libs
+do.import utils.tokens utils.configs
+
 ### Find all Cloud Function definition files ###
 # usage: findDefinitions <absolute functions folder path> <cloud function definition filename>
 function findDefinitions() {
@@ -43,16 +46,14 @@ function getDefinitions() {
 ### Prepare for Deploying Cloud Function ###
 # usage: prepareForDeploy <functions folder> <aditional parameters array list>
 function prepareForDeploy() {
-    # Import required libs
-    do.import utils.tokens utils.configs
 
     getArgs "FUNCTIONS_FOLDER CF_DEFINITIONS_FILE ENV_YML_SOURCE_FILE" "${@}"
 
     # Find all Cloud Function definition files
     findDefinitions ${FUNCTIONS_FOLDER} ${CF_DEFINITIONS_FILE}
 
-    # Retrieving the definition files found
-    CF_DEFINITIONS_FILES=($(do.gcp.function.getDefinitions))
+    # Retrieving the definition files found do.gcp.function.
+    CF_DEFINITIONS_FILES=($(getDefinitions))
 
     for cfDefFiles in ${CF_DEFINITIONS_FILES[@]}; do
         CF_ROOTDIR=$(dirname ${cfDefFiles})
