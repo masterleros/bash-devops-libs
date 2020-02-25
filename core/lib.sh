80283a75ae487d078bf4c185e75c7a45ebdf5599
Leonardo Morales <leonardo.morales@gft.com>
2020-02-25 15:34:53 -0500

    codacy code quality improvement

--- core/lib.sh
+++ core/lib.sh
@@ -15,12 +15,12 @@
 #!/bin/bash
 
 # Include the header
-CURRENT_LIB=${1}
-CURRENT_LIB_DIR=${2}
-CURRENT_LIB_ENTRYPOINT=${3}
+local CURRENT_LIB=${1}
+local CURRENT_LIB_DIR=${2}
+local CURRENT_LIB_ENTRYPOINT=${3}
 
 # Include main code
-. ${CURRENT_LIB_ENTRYPOINT} || return ${?}
+. "${CURRENT_LIB_ENTRYPOINT}" || return ${?}
 
 # Include the footer (deprecated)
 # if [ $(basename ${0}) == $(basename ${BASH_SOURCE[0]}) ]; then 
